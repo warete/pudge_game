c5ccd701dac35a8fb7c19dc4b6d88d66e3886042
Egor Borisovsky <egor@intervolga.ru>
2021-01-06 18:04:00 +0300

    sounds

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 import utils
 import os
+import random
 from enemy import get_random_enemy
 
 
@@ -21,7 +22,11 @@ class Game:
     points = 0
     isGameStarted = False
 
+    sounds = {}
+
     def __init__(self, max_fps=30, window_sizes=(600, 700), field_cells_count=5):
+        self.sounds_dir = 'sounds'
+        self.sounds_obj = {}
         self.max_fps = max_fps
         self.window_sizes = window_sizes
         self.field_cells_count = field_cells_count
@@ -35,6 +40,8 @@ class Game:
         self.screen = pygame.display.set_mode(self.window_sizes)
         self.clock = pygame.time.Clock()
 
+        self.set_sounds()
+
         self.optimus_font_big = pygame.font.Font(os.path.join('fonts', 'OptimusPrinceps.ttf'), 120)
         self.optimus_font = pygame.font.Font(os.path.join('fonts', 'OptimusPrinceps.ttf'), 35)
 
@@ -47,6 +54,8 @@ class Game:
         self.lifes = 3
         self.points = 0
         self.reload_enemy_and_field()
+        for i in range(len(self.sounds_obj['fail'])):
+            self.sounds_obj['fail'][i].stop()
 
     def reload_enemy_and_field(self):
         self.field = utils.get_new_field(self.field_cells_count)
@@ -54,6 +63,28 @@ class Game:
         self.enemy = get_random_enemy()
         self.enemy.set_sizes(self.field_cell_size, self.field_cell_size)
         self.enemy.set_coords(enemy_x, enemy_y)
+
+    def set_sounds(self):
+        self.sounds = {
+            'missed': [
+                'missed.wav',
+                'missed2.wav',
+            ],
+            'fail': [
+                'deathsouls.wav',
+                'explosion.wav'
+            ],
+            'hit': [
+                'hit.wav',
+                'hit2.wav',
+                'hit3.wav',
+            ]
+        }
+        for key in self.sounds:
+            self.sounds_obj[key] = []
+            for i in range(len(self.sounds[key])):
+                self.sounds_obj[key].append(
+                    pygame.mixer.Sound(os.path.join(os.path.abspath('.'), self.sounds_dir, self.sounds[key][i])))
 
     def handle_events(self):
         for event in pygame.event.get():
@@ -66,14 +97,19 @@ class Game:
                 if self.isGameStarted:
                     if self.enemy.get_rect().collidepoint(pos[0], pos[1]):
                         self.lifes, self.points = self.enemy.on_click(self.lifes, self.points)
+                        self.sounds_obj['hit'][random.randint(0, len(self.sounds_obj['hit']) - 1)].play()
                     else:
                         self.lifes -= 1
+                        self.sounds_obj['missed'][random.randint(0, len(self.sounds_obj['missed']) - 1)].play()
                 else:
                     self.start_game()
 
     def check_game_over(self):
         if self.lifes == 0:
             self.isGameStarted = False
+
+            for i in range(len(self.sounds_obj['fail'])):
+                self.sounds_obj['fail'][i].play()
 
             points_text = self.optimus_font.render('Pudges banned: ' + str(self.points), False, (173, 0, 0))
             self.screen.blit(points_text, points_text.get_rect(center=(self.window_sizes[0] / 2, 35)))
